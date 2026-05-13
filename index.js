4bff2b156646757dc54728c1f9d5852ad57345bc
Danadyaksa <aksnonymous@gmail.com>
2026-05-13 16:37:01 +0700

    ganti index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
 const noteRoutes = require("./routes/noteRoutes");
+
+// Panggil koneksi database
+const sequelize = require("./config/database"); 
 
 const app = express();
 
@@ -17,9 +19,16 @@ app.get("/", (req, res) => {
 // Routes utama
 app.use("/api/v1/notes", noteRoutes);
 
-
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// 2. Mantra Sequelize untuk auto-create tabel sebelum server nyala
+sequelize.sync()
+  .then(() => {
+    console.log("Database & Tabel berhasil disinkronisasi!");
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Gagal sinkronisasi database:", err);
+  });
